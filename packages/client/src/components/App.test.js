4f656ba32d064d7f5994ac687ac1c9ca11d0a4ba
Vladimir Cutkovic <vladimir.cutkovic@gmail.com>
2021-04-04 03:50:22 +0200

    Add tests for actions Add tests from reducers

--- packages/client/src/components/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders home page element', () => {
-  render(<App />);
-  const homePageElement = screen.getByText(/Home/i);
-  expect(homePageElement).toBeInTheDocument();
-});
---
+++ packages/client/src/store/convertor/__tests__/actions.test.js
@@ -0,0 +1,62 @@
+import * as actions from '../actions';
+import * as types from '../types';
+
+describe('actions', () => {
+	describe('roman to integer', () => {
+		it('should create convert action', () => {
+			const payload = 'MMC';
+			const expectedAction = {
+				type: types.convertRomanToInteger,
+				payload,
+			};
+			expect(actions.convertRomanToInteger(payload)).toEqual(expectedAction);
+		});
+
+		it('should create success action', () => {
+			const payload = '2100';
+			const expectedAction = {
+				type: types.convertRomanToIntegerSuccess,
+				payload,
+			};
+			expect(actions.convertRomanToIntegerSuccess(payload)).toEqual(expectedAction);
+		});
+
+		it('should create error action', () => {
+			const payload = 'There was an error';
+			const expectedAction = {
+				type: types.convertRomanToIntegerError,
+				payload,
+			};
+			expect(actions.convertRomanToIntegerError(payload)).toEqual(expectedAction);
+		});
+	});
+
+	describe('integer to roman', () => {
+		it('should create convert action', () => {
+			const payload = 10;
+			const expectedAction = {
+				type: types.convertIntegerToRoman,
+				payload,
+			};
+			expect(actions.convertIntegerToRoman(payload)).toEqual(expectedAction);
+		});
+
+		it('should create success action', () => {
+			const payload = 'MMC';
+			const expectedAction = {
+				type: types.convertIntegerToRomanSuccess,
+				payload,
+			};
+			expect(actions.convertIntegerToRomanSuccess(payload)).toEqual(expectedAction);
+		});
+
+		it('should create an error action', () => {
+			const payload = 'There was an error';
+			const expectedAction = {
+				type: types.convertIntegerToRomanError,
+				payload,
+			};
+			expect(actions.convertIntegerToRomanError(payload)).toEqual(expectedAction);
+		});
+	});
+});
